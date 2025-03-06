a3b9818410f26378faa03e46a5273f7e0113cc08
jeoungsung12 <jeoungsung12@naver.com>
2025-03-06 14:57:04 +0900

    Add: Resource Init Setting

---
+++ CryptoApp/Scene/TabBar/TabBarController.swift
@@ -0,0 +1,17 @@
+//
+//  TabBarController.swift
+//  CryptoApp
+//
+//  Created by 정성윤 on 3/6/25.
+//
+
+import UIKit
+
+final class TabBarController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
